e9a93755e6619eb9131d8a28c16c2c90aeaa3d24
danie665 <danie665@umn.edu>
2017-08-01 13:21:27 -0500

    update random number documentation

--- random_number.py
+++ random_number.py
@@ -1,6 +1,10 @@
 import random
 
+# this script outputs four random integers between 1 and 9 for an
+# arithmetic game
+
 def random_array():
+    # create array of four random numbers
     return_array = []
     while len(return_array) < 4:
         random_number = random.randint(1, 9)
