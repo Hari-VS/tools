18ca1f395425bf0612a8ebbd8f4c535ab7fb84b8
danie665 <danie665@umn.edu>
2017-09-07 11:27:11 -0500

    adding my cmd_env file

---
+++ taylor_env.cmd
@@ -0,0 +1,23 @@
+title "Taylor's Environment"
+
+doskey blng="C:\Program Files (x86)\PuTTY\putty.exe" -load BLNG
+
+doskey chrome="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+
+doskey excel="C:\Program Files (x86)\Microsoft Office\Office15\EXCEL.EXE"
+
+doskey ie="C:\Program Files\Internet Explorer\iexplore.exe"
+
+doskey gridshell="C:\Program Files (x86)\PuTTY\putty.exe" -load GridShell
+
+doskey mis="C:\Program Files (x86)\PuTTY\putty.exe" -load MIS
+
+doskey ny1corebrain01="C:\Program Files (x86)\PuTTY\putty.exe" -load ny1corebrain01
+
+doskey ny1coreclnt01="C:\Program Files (x86)\PuTTY\putty.exe" -load ny1coreclnt01
+
+doskey outlook="C:\Program Files (x86)\Microsoft Office\Office15\OUTLOOK.EXE"
+
+doskey ppt="C:\Program Files (x86)\Microsoft Office\Office15\POWERPNT.EXE"
+
+doskey word="C:\Program Files (x86)\Microsoft Office\Office15\WINWORD.EXE"
